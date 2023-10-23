e17444635f35d4565ec203395e0db712eeb74fef
Yasmin Aguirre <yas.cardozo.aguirre@gmail.com>
2023-10-23 18:01:43 -0300

    adicionando um pouco de javadoc

--- T2_ALEST/src/DoubleLinkedListOfInteger.java
+++ T2_ALEST/src/DoubleLinkedListOfInteger.java
@@ -1,8 +1,20 @@
+/**
+ * A classe DoubleLinkedListOfInteger representa uma lista de inteiros duplamente encadeada.
+ * Dentro dessa classe nós temos a classe Node que representa um nodo que irá fazer parte da lista duplamente encadeada.
+ * A classe Node tem como atributos o valor(int) que será armazenado dentro do nodo e as referências para o nodo anterior e próximo.
+ * 
+ * @author 4gu1rr3 (Yasmin Aguirre)
+ * @version 1.0
+*/
 public class DoubleLinkedListOfInteger {
     public class Node{
         private int item;
         private Node next;
         private Node prev;
+        /**
+         * Construtor da classe Node.
+         * @param item
+        */
         public Node(int item){
             this.item = item;
         }
@@ -10,12 +22,17 @@ public class DoubleLinkedListOfInteger {
 
     private Node head;
     private Node tail;
-    
+    /**
+     * Construtor da classe DoubleLinkedListOfInteger.
+    */
     public DoubleLinkedListOfInteger(){
         head = null;
         tail = null;
     }
-
+    /**
+     * Adiciona um nodo a lista.
+     * @param item a ser adicionado na lista
+    */
     public void add(int item){
         Node newNode = new Node(item);
         if(head == null){
@@ -27,6 +44,10 @@ public class DoubleLinkedListOfInteger {
             tail = newNode;
         }
     }
+    /**
+     * Remove o último nodo da lista.
+     * @return true se o nodo foi removido, false se não.
+    */
     public boolean removeLastNode(){
         if(head == null){
             return false;
@@ -40,6 +61,10 @@ public class DoubleLinkedListOfInteger {
         tail.next = null;
         return true;
     }
+    /**
+     * Remove o primeiro nodo da lista.
+     * @return true se o nodo foi removido, false se não.
+     */
     public boolean removeFirstNode(){
         if(head == null){
             return false;
@@ -53,6 +78,11 @@ public class DoubleLinkedListOfInteger {
         head.prev = null;
         return true;
     }
+    /**
+     * Remove um nodo da lista.
+     * @param item a ser removido da lista. 
+     * @return true se foi removido, false se não.
+     */
     public boolean removeNode(int item){
         if(head == null){
             return false;
