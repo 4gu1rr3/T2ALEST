e3a469841127ba92f7347fe4d88fba0e22842bb6
Yasmin Aguirre <yas.cardozo.aguirre@gmail.com>
2023-10-23 18:30:05 -0300

    Modificando lógica para ter sentinelas

--- T2_ALEST/src/DoubleLinkedListOfInteger.java
+++ T2_ALEST/src/DoubleLinkedListOfInteger.java
@@ -7,109 +7,99 @@
  * @version 1.0
 */
 public class DoubleLinkedListOfInteger {
+    private Node header;
+    private Node trailer;
+    private Node current;
+    private Integer count;
+
+    /**
+     * Classe Node representa um nodo que irá fazer parte da lista duplamente encadeada.
+     * A classe Node tem como atributos o valor(int) que será armazenado dentro do nodo e as referências para o nodo anterior e próximo.
+    */
     public class Node{
-        private int item;
+        private Integer item;
         private Node next;
         private Node prev;
         /**
          * Construtor da classe Node.
-         * @param item
+         * @param item item que vai ser guardado no nodo
         */
-        public Node(int item){
+        public Node(Integer item){
             this.item = item;
+            this.next = null;
+            this.prev = null;
         }
     }
 
-    private Node head;
-    private Node tail;
     /**
      * Construtor da classe DoubleLinkedListOfInteger.
     */
     public DoubleLinkedListOfInteger(){
-        head = null;
-        tail = null;
+        header = new Node(null);
+        trailer = new Node(null);
+        header.next = trailer;
+        trailer.prev = header;
+        count = 0;
     }
+
     /**
-     * Adiciona um nodo a lista.
-     * @param item a ser adicionado na lista
+     * Adiciona um nodo ao final da lista.
+     * @param item item a ser adicionado na lista
     */
-    public void add(int item){
+    public void add(Integer item){
         Node newNode = new Node(item);
-        if(head == null){
-            head = newNode;
-            tail = newNode;
-        }else{
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
+        newNode.prev = trailer.prev;
+        newNode.next = trailer;
+        trailer.prev.next = newNode;
+        trailer.prev = newNode;
+        count++;
     }
+
     /**
      * Remove o último nodo da lista.
      * @return true se o nodo foi removido, false se não.
     */
     public boolean removeLastNode(){
-        if(head == null){
+        if(count == 0){
             return false;
         }
-        if(head == tail){
-            head = null;
-            tail = null;
-            return true;
-        }
-        tail = tail.prev;
-        tail.next = null;
+        trailer.prev.prev.next = trailer;
+        trailer.prev = trailer.prev.prev;
+        count--;
         return true;
     }
+
     /**
      * Remove o primeiro nodo da lista.
      * @return true se o nodo foi removido, false se não.
      */
     public boolean removeFirstNode(){
-        if(head == null){
+        if(count == 0){
             return false;
         }
-        if(head == tail){
-            head = null;
-            tail = null;
-            return true;
-        }
-        head = head.next;
-        head.prev = null;
+        header.next.next.prev = header;
+        header.next = header.next.next;
+        count--;
         return true;
     }
+
     /**
      * Remove um nodo da lista.
-     * @param item a ser removido da lista. 
+     * @param item item a ser removido da lista. 
      * @return true se foi removido, false se não.
      */
-    public boolean removeNode(int item){
-        if(head == null){
-            return false;
-        }
-        if(head == tail){
-            if(head.item == item){
-                head = null;
-                tail = null;
-                return true;
-            }else{
-                return false;
+    public boolean removeNode(Integer item){
+        if(count > 0){
+            current = header.next;
+            while(current.item!= null){
+                if(current.item.equals(item)){
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                    count--;
+                    return true;
+                }
+                current = current.next;
             }
-        }
-        if(head.item == item){
-            removeFirstNode();
-        }
-        if(tail.item == item){
-            removeLastNode();
-        }
-        Node current = head.next;
-        while(current!= null){
-            if(current.item == item){
-                current.prev.next = current.next;
-                current.next.prev = current.prev;
-                return true;
-            }
-            current = current.next;
         }
         return false;
     }
